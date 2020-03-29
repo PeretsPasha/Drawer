6854186d20debd92e0d4caee9cd2da84b30586b4
PeretsPasha <perets.pasha.pro@gmail.com>
2020-03-29 14:18:24 +0300

    New commit

--- app/src/main/java/com/example/drawer/ChatFragment.java
+++ app/src/main/java/com/example/drawer/ChatFragment.java
@@ -3,29 +3,39 @@ package com.example.drawer;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ChatFragment extends Fragment {
-
+public class ChatFragment extends Fragment implements View.OnClickListener{
 
     public ChatFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View root =  inflater.inflate(R.layout.fragment_chat, container, false);
+        Button button = (Button) root.findViewById(R.id.button);
+        button.setOnClickListener(this);
+        return root;
     }
 
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getActivity(), "Вы нажали на кнопку",
+                Toast.LENGTH_SHORT).show();
+    }
 }
